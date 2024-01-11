a4e11f5aa1264d06a7045b9a7d9c4e2bbe207344
jose-pedro <josepedro.bernardes1@gmail.com>
2024-01-11 12:22:49 -0300

    chore: remontando database

---
+++ src/database/data.ts
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    img: '/images/cadeira.svg',
+    description:
+      'Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.',
+    price: 'de R$ 582,90',
+    salePrice: 'R$ 238,41',
+    installments: '5x de R$ 47,68 sem juros',
+  },
+  {
+    id: 2,
+    img: '/images/relogio.svg',
+    description:
+      'Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.',
+    price: 'de R$ 582,90',
+    salePrice: 'R$ 238,41',
+    installments: '5x de R$ 47,68 sem juros',
+  },
+  {
+    id: 3,
+    img: '/images/pa.svg',
+    description:
+      'Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.',
+    price: 'de R$ 582,90',
+    salePrice: 'R$ 238,41',
+    installments: '5x de R$ 47,68 sem juros',
+  },
+  {
+    id: 4,
+    img: '/images/cadeiraBranca.svg',
+    description:
+      'Aliquam congue nisl ele ac ligula convallis, luctus blandit augue.',
+    price: 'de R$ 582,90',
+    salePrice: 'R$ 238,41',
+    installments: '5x de R$ 47,68 sem juros',
+  },
+]
